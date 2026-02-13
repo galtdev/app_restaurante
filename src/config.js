5bdc95121847d054f2d8dfa0673040ecaf54be25
Angel Osuna <angeldanielosunafiguera@gmail.com>
2026-02-12 22:18:12 -0400

    cambio de background de form_container

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ module.exports = {
     mysql: {
         host: process.env.MYSQL_HOST || 'localhost',
         user: process.env.MYSQL_USER || 'root',
-        password: process.env.MYSQL_PASSWORD || 'clave',
+        password: process.env.MYSQL_PASSWORD || '',
         database: process.env.MYSQL_DB || 'restaurante'
     }
 }
